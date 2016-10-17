f52d8a33ac919b286b50e915db6eb0fc7872d020
Sam Quinn <lazersmoke@gmail.com>
2016-10-17 16:48:30 -0500

    Clicky Wizard

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-df
+gameState = function(){
+  this.gold = 0
+  changeGold = function(dGold){
+    this.gold += dGold
+  }
+  return {changeGold}
+}()
+ 
+mainButtonClick = function(){
+  gameState.gold++
+}
