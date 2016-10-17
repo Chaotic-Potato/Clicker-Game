b1e14082a2eef45040d78ae9f52d744b74d48825
Chaotic-Potato <andy825825@gmail.com>
2016-10-17 16:58:26 -0500

    Adds support of increase over time

--- game.js
+++ game.js
@@ -1,7 +1,20 @@
+var Game = {
+  const TICK_RATE = 60,
+  tick: function() {
+    gameState.goldTick()
+  }
+}
+
 gameState = function(){
   this.gold = 0
   changeGold = function(dGold){
     this.gold += dGold
+  },
+  goldTick = function() {
+    this.changeGold(this.getGps() / game.tickRate)
+  },
+  getGps = function() {
+    return 0
   }
   return {changeGold}
 }()
